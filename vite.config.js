4f31277112a568868238c908c763dfa4f334f929
Jagadeesh <jagadeeshwaran@Jagadeeshs-MacBook-Air.local>
2026-03-08 12:47:16 +0530

    new update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '/',
+  base: '/',
   build: {
     chunkSizeWarningLimit: 1000,
   },
